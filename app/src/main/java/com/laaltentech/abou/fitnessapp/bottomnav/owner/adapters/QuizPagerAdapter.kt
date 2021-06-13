5eee256703b3d3b74042cbecd09ea36c97110e3a
Abou Zuhayr <you@example.com>
2021-06-13 21:28:04 +0530

    the view is approximately made

--- app/src/main/java/com/laaltentech/abou/fitnessapp/bottomnav/owner/adapters/QuizPagerAdapter.kt
+++ app/src/main/java/com/laaltentech/abou/fitnessapp/bottomnav/owner/adapters/QuizPagerAdapter.kt
@@ -1,8 +1,12 @@
 package com.laaltentech.abou.fitnessapp.bottomnav.owner.adapters
 
+import android.animation.LayoutTransition
+import android.content.res.Resources
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.LinearLayout
 import androidx.databinding.DataBindingComponent
 import androidx.databinding.DataBindingUtil
@@ -28,7 +32,25 @@ class QuizPagerAdapter(private val dataBindingComponent: DataBindingComponent?, 
                 false,
                 dataBindingComponent
             )
+
+        binding.materialCardViewA.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+        binding.materialCardViewB.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+        binding.materialCardViewC.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+        binding.layoutLinear.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+        binding.constraintA.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+        binding.constraintB.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+        binding.constraintC.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
+
         container.addView(binding.root)
+        binding.textData.text = questions[position].questionObservation
+
+        binding.materialCardViewA.setOnClickListener {
+            val params: ViewGroup.LayoutParams = binding.materialCardViewA.layoutParams
+            params.height = (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100f, container.context.resources.displayMetrics)).toInt()
+            it.backgroundTintList = container.context.resources.getColorStateList(R.color.dot_inactive_screen2)
+            binding.optionAArrow.setImageResource(R.drawable.ic_correct)
+            binding.textData.text = "button was clicked"
+        }
         return binding.root
     }
 
