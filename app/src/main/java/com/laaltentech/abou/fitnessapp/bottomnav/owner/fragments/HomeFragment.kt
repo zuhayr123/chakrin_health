ee541c5570369e28712ddfa909b994ec21ff3c9d
Abou Zuhayr <abouzuhayr@Abous-MacBook-Air-2.local>
2022-02-25 16:41:37 +0530

    basic app with razorpay maven repo added

--- app/src/main/java/com/laaltentech/abou/fitnessapp/bottomnav/owner/fragments/HomeFragment.kt
+++ app/src/main/java/com/laaltentech/abou/fitnessapp/bottomnav/owner/fragments/HomeFragment.kt
@@ -4,11 +4,13 @@ import android.app.ActivityManager
 import android.content.Context
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.*
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -29,6 +31,8 @@ class HomeFragment : Fragment(), Injectable {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    var counter = 0
+
     var dataBindingComponent = FragmentDataBindingComponent(this)
 
     lateinit var binding: FragmentHomeBinding
@@ -48,7 +52,7 @@ class HomeFragment : Fragment(), Injectable {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         (activity as (BottomMainNavActivity)).binding.navView.visibility = View.VISIBLE
 
-        val sharedPref = this.activity!!.getSharedPreferences("pref", Context.MODE_PRIVATE)
+        val sharedPref = this.requireActivity().getSharedPreferences("pref", Context.MODE_PRIVATE)
         val firstName = sharedPref.getString(CONSTANTS.FIRST_NAME, "")
         if(!firstName.isNullOrEmpty()){
             binding.textView.text = "Welcome $firstName!!"
@@ -77,6 +81,8 @@ class HomeFragment : Fragment(), Injectable {
             val action = HomeFragmentDirections.actionNavigationHomeToFragmentOthersWorkout()
             findNavController().navigate(action)
         }
+
+        autoRefresh(requireContext());
         super.onActivityCreated(savedInstanceState)
     }
 
@@ -118,5 +124,69 @@ class HomeFragment : Fragment(), Injectable {
             (context?.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
                 .clearApplicationUserData() // note: it has a return value!
         }, 1000)
+    }
+
+    fun autoRefresh(context: Context) {
+        val h = Handler()
+        val delay : Long = 4000 //milliseconds
+        h.postDelayed(Runnable {
+            Log.e("Counter", "The counter value is $counter")
+            if(activity != null){
+                when(counter){
+                    1 -> {
+                        binding.textView3.text = "Limit sugar,caffeine and processed foods."
+                        binding.textView3.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.fade_accelerated))
+                    }
+
+                    2 -> {
+                        binding.textView3.text = "Snack on local seasonal fruits & nuts."
+                        binding.textView3.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.fade_accelerated))
+                    }
+
+                    3 -> {
+                        binding.textView3.text = "Walk for atleast 10 mins after main meals."
+                        binding.textView3.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.fade_accelerated))
+                    }
+
+                    4 -> {
+                        binding.textView3.text = "Only drink warm water after 45 mins of any meal. Little sips can be taken between meals if thirsty."
+                        binding.textView3.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.fade_accelerated))
+                    }
+
+                    5 -> {
+                        binding.textView3.text = "Get atleast 2 hours of movement everyday - 1 hour in the morning & 1 hour in the evening."
+                        binding.textView3.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.fade_accelerated))
+                    }
+
+                    6 -> {
+                        binding.textView3.text = "Exercise in fresh air."
+                        binding.textView3.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.fade_accelerated))
+                    }
+
+                    7 -> {
+                        binding.textView3.text = "Never exercise on a full stomach."
+                        binding.textView3.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.fade_accelerated))
+                    }
+
+                    8 -> {
+                        binding.textView3.text = "Don't use technology after 10 pm."
+                        binding.textView3.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.fade_accelerated))
+                    }
+
+                    9 -> {
+                        binding.textView3.text = "Be thankful for the food you eat."
+                        binding.textView3.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.fade_accelerated))
+                    }
+
+                    10 -> {
+                        binding.textView3.text = "Eat your meals without any distraction."
+                        binding.textView3.startAnimation(AnimationUtils.loadAnimation(activity, R.anim.fade_accelerated))
+                    }
+                }
+                counter += 1
+                //Activity written here will be called every 2 sec
+                autoRefresh(context = context)
+            }
+        }, delay)
     }
 }
